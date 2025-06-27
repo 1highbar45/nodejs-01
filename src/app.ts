28e17fab7efb69a53510bf1b80efda97108d7c32
1highbar45 <lhlvhsg@gmail.com>
2025-06-28 00:28:40 +0700

    view template

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import 'dotenv/config'
 const app = express()
 const port = process.env.PORT || 8080
 
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.render('home.ejs')
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
