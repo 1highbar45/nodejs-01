97616a05f06710de03dd27129d10df0e6130b7a3
1highbar45 <lhlvhsg@gmail.com>
2025-07-20 23:15:36 +0700

    filter and sort

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { countTotalProductClientPages, getProducts } from "services/client/item.service";
+import { getProductWithFilter } from "services/client/product.filter";
 import { getAllRoles, getAllUsers, getUserById, handleCreateUser, handleDeleteUser, updateUserById } from "services/user.service";
+import { StringValidation } from "zod";
 
 const getHomePage = async (req: Request, res: Response) => {
     const { page } = req.query;
@@ -19,17 +21,26 @@ const getHomePage = async (req: Request, res: Response) => {
 };
 
 const getProductFilterPage = async (req: Request, res: Response) => {
-    const { page } = req.query;
+    const { page, factory = "", target = "", price = "", sort = "" } =
+        req.query as {
+            page?: string;
+            factory: string;
+            target: string;
+            price: string;
+            sort: string;
+        };
 
     let currentPage = page ? +page : 1;
     if (currentPage <= 0) currentPage = 1;
 
-    const totalPages = await countTotalProductClientPages(6);
+    // const totalPages = await countTotalProductClientPages(6);
+    // const products = await getProducts(currentPage, 6);
 
-    const products = await getProducts(currentPage, 6);
+    const data = await getProductWithFilter(currentPage, 6, factory, target, price, sort);
+
     return res.render('client/product/filter.ejs', {
-        products,
-        totalPages: +totalPages,
+        products: data.products,
+        totalPages: +data.totalPages,
         page: +currentPage
     })
 };
