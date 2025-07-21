0943cf2242fe0d20ad5f9ce3dd82fce56b24a02c
1highbar45 <lhlvhsg@gmail.com>
2025-07-21 14:49:00 +0700

    rest api

---
+++ src/routes/api.ts
@@ -0,0 +1,18 @@
+import { createUsersAPI, deleteUserByIdAPI, getAllUsersAPI, getUserByIdAPI, postAddProductToCartAPI, updateUserByIdAPI } from 'controllers/client/api.controller';
+import express, { Express } from 'express';
+
+const router = express.Router()
+
+const apiRoutes = (app: Express) => {
+    router.post("/add-product-to-cart", postAddProductToCartAPI);
+
+    router.get("/users", getAllUsersAPI);
+    router.get("/users/:id", getUserByIdAPI);
+    router.post("/users", createUsersAPI);
+    router.put("/users/:id", updateUserByIdAPI)
+    router.delete("/users/:id", deleteUserByIdAPI)
+
+    app.use("/api", router);
+}
+
+export default apiRoutes;
